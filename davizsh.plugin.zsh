1d18913694506ad396b77fa6284334391f4d20f5
Davi <davicoradini@gmail.com>
2017-07-06 09:11:55 -0300

    More shortcuts

--- davizsh.plugin.zsh
+++ davizsh.plugin.zsh
@@ -5,6 +5,11 @@ alias cdznot='cd ~/PycharmProjects/zanzarguias-notifier/'
 alias cdzweb='cd ~/PycharmProjects/zanzarguias-app-web/'
 alias cdzman='cd ~/PycharmProjects/zanzarguias-management-app-web'
 alias cdzand='cd ~/AndroidStudioProjects/ZanzarGuias'
+alias srczapi='source ~/.virtualenvs/zanzarguias-api/bin/activate'
+alias srcznot='source ~/.virtualenvs/zanzarguias-notifier/bin/activate'
 
 alias cdfapi='cd ~/PycharmProjects/fideliza-facil-api'
 alias cdfand='cd ~/AndroidStudioProjects/FidelizaFacil'
+alias srcfapi='source ~/.virtualenvs/fidelizafacil-api/bin/activate'
+
+alias dockerall='docker start rabbit-production mysql-local'
