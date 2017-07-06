1f5ddf619b53fa663ab6ae5ccc4d8b011a3a746a
Davi <davicoradini@gmail.com>
2017-07-06 09:05:05 -0300

    more shortcuts

--- davizsh.plugin.zsh
+++ davizsh.plugin.zsh
@@ -4,4 +4,7 @@ alias cdzapi='cd ~/PycharmProjects/zanzarguias-api/'
 alias cdznot='cd ~/PycharmProjects/zanzarguias-notifier/'
 alias cdzweb='cd ~/PycharmProjects/zanzarguias-app-web/'
 alias cdzman='cd ~/PycharmProjects/zanzarguias-management-app-web'
+alias cdzand='cd ~/AndroidStudioProjects/ZanzarGuias'
 
+alias cdfapi='cd ~/PycharmProjects/fideliza-facil-api'
+alias cdfand='cd ~/AndroidStudioProjects/FidelizaFacil'
