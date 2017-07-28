0fc859142fec13db27194bda3b223c7433d44168
Davi <davicoradini@gmail.com>
2017-07-28 15:13:45 -0300

    atalhos quiteja ja

--- davizsh.plugin.zsh
+++ davizsh.plugin.zsh
@@ -1,15 +1,28 @@
 #!/bin/zsh
 
 alias cdzapi='cd ~/PycharmProjects/zanzarguias-api/'
-alias cdznot='cd ~/PycharmProjects/zanzarguias-notifier/'
+alias cdzasy='cd ~/PycharmProjects/zanzarguias-async/'
 alias cdzweb='cd ~/PycharmProjects/zanzarguias-app-web/'
 alias cdzman='cd ~/PycharmProjects/zanzarguias-management-app-web'
 alias cdzand='cd ~/AndroidStudioProjects/ZanzarGuias'
 alias srczapi='source ~/.virtualenvs/zanzarguias-api/bin/activate'
-alias srcznot='source ~/.virtualenvs/zanzarguias-notifier/bin/activate'
+alias srczasy='source ~/.virtualenvs/zanzarguias-async/bin/activate'
 
 alias cdfapi='cd ~/PycharmProjects/fideliza-facil-api'
 alias cdfand='cd ~/AndroidStudioProjects/FidelizaFacil'
 alias srcfapi='source ~/.virtualenvs/fidelizafacil-api/bin/activate'
+
+alias cdqapi='cd ~/PycharmProjects/quiteja-api/'
+alias cdqdea='cd ~/PycharmProjects/quiteja-deal-app-web/'
+alias cdqimp='cd ~/PycharmProjects/quiteja-importer/'
+alias cdqlan='cd ~/PycharmProjects/quiteja-landing-page/'
+alias cdqlib='cd ~/PycharmProjects/quiteja-lib-py/'
+alias cdqman='cd ~/PycharmProjects/quiteja-management-app-web/'
+alias cdqnot='cd ~/PycharmProjects/quiteja-notifier/'
+alias srcqapi='source ~/.virtualenvs/quiteja-api/bin/activate'
+alias srcqimp='source ~/.virtualenvs/quiteja-importer/bin/activate'
+alias srcqlan='source ~/.virtualenvs/quiteja-landing-page/bin/activate'
+alias srcqlib='source ~/.virtualenvs/quiteja-lib/bin/activate'
+alias srcqnot='source ~/.virtualenvs/quiteja-notifier/bin/activate'
 
 alias dockerall='docker start rabbit-production mysql-local'
